22acedac510ca9937de781063a384b6d1ca60bfd
4Leonid <leo-turko@mail.ru>
2022-03-23 16:44:45 +0300

    Service file settings

---
+++ ContactList/Services/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  ContactList
+//
+//  Created by EkaterinaP on 23.03.2022.
+//
+
+class DataManager {
+    var firstNames = ["Leo", "Alexey", "Ivan", "Michael", "Alexandr","Vladimir","Sergey"]
+    var secondNames = ["Turko", "Belov", "Ivanov", "Kruglov", "Pavlov", "Rublev", "Bobrov"]
+    var phones = ["1111", "2222", "3333", "4444", "5555", "6666", "7777"]
+    var eMails = ["a@mail.ru", "b@mail.ru", "c@mail.ru", "d@mail.ru", "e.@mail.ru", "f@mail.ru", "g@mail.ru"]
+}
